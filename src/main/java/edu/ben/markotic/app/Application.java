86e97a7410a6854651e45803595ed27cbb99de07
Joel Sandoval <superpiojo17@gmail.com>
2017-11-28 19:58:05 -0600

    first

--- src/main/java/edu/ben/markotic/app/Application.java
+++ src/main/java/edu/ben/markotic/app/Application.java
@@ -1,4 +1,4 @@
-package edu.ben.rate_review.app;
+package edu.ben.markotic.app;
 
 import static spark.Spark.*;
 
@@ -6,9 +6,8 @@ import static spark.Spark.*;
  * This application class runs our entire project and is also where we configure the paths to get to all our pages
  */
 
-import java.util.HashMap;
 
-import edu.ben.rate_review.controller.home.*;
+import edu.ben.markotic.controller.home.*;
 import spark.Session;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -18,21 +17,9 @@ public class Application {
 	public static boolean ALLOW_EMAIL = true;
 	// SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
 	// "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"
-	public static String DOMAIN = "rateandreview.herokuapp.com";
 	// match up paths
 	public static String HOME_PATH = "/";
 	public static String NOTFOUND_HOME_PATH = "/notfound";
-	public static String CALENDAR_PATH = HOME_PATH + "calendar";
-
-	public static String SCHEDULE_PATH = HOME_PATH + "schedule";
-	public static String WELCOME_PATH = HOME_PATH + "welcome";
-	public static String RULES_PATH = HOME_PATH + "rules";
-
-	public static String ABOUTME_PATH = HOME_PATH + "aboutme";
-	public static String OTHER_PATH = HOME_PATH + "other";
-	public static String DATA_PATH = HOME_PATH + "data";
-	public static String BEHAVIOR_PATH = HOME_PATH + "behavior";
-	public static String OURCLASSROOM_PATH = HOME_PATH + "ourclassroom";
 
 	public static void main(String[] args) throws Exception {
 
@@ -89,33 +76,6 @@ public class Application {
 		get(NOTFOUND_HOME_PATH, HomeController::showHomePage, new HandlebarsTemplateEngine());
 
 		// path for homework
-
-		get(CALENDAR_PATH, HomeController::showCalendar, new HandlebarsTemplateEngine());
-
-		// path for class schedule
-		get(SCHEDULE_PATH, HomeController::showClassSchedule, new HandlebarsTemplateEngine());
-
-		// path for supplies
-		get(WELCOME_PATH, HomeController::showWelcome, new HandlebarsTemplateEngine());
-
-		// path for rules
-		get(RULES_PATH, HomeController::showRules, new HandlebarsTemplateEngine());
-
-		// path for about me
-		get(ABOUTME_PATH, HomeController::showAboutMe, new HandlebarsTemplateEngine());
-
-		// path for show other
-		get(OTHER_PATH, HomeController::showOther, new HandlebarsTemplateEngine());
-
-		// path for data
-
-		get(DATA_PATH, HomeController::showData, new HandlebarsTemplateEngine());
-
-		// path for behavior
-		get(BEHAVIOR_PATH, HomeController::showBehavior, new HandlebarsTemplateEngine());
-
-		// path for our classroom
-		get(OURCLASSROOM_PATH, HomeController::showOurClassroom, new HandlebarsTemplateEngine());
 
 	}
 }
--- src/main/java/edu/ben/markotic/controller/home/HomeController.java
+++ src/main/java/edu/ben/markotic/controller/home/HomeController.java
@@ -1,11 +1,10 @@
-package edu.ben.rate_review.controller.home;
+package edu.ben.markotic.controller.home;
 
 import java.util.HashMap;
 
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
-import spark.Session;
 
 public class HomeController {
 
@@ -14,70 +13,6 @@ public class HomeController {
 		HashMap<String, Object> model = new HashMap<>();
 
 		return new ModelAndView(model, "home/home.hbs");
-	}
-	
-	public static ModelAndView showCalendar(Request req, Response res) throws Exception {
-		// Just a hash to pass data from the servlet to the page
-		HashMap<String, Object> model = new HashMap<>();
-
-		return new ModelAndView(model, "home/calendar.hbs");
-	}
-
-
-	public static ModelAndView showClassSchedule(Request req, Response res) throws Exception {
-		// Just a hash to pass data from the servlet to the page
-		HashMap<String, Object> model = new HashMap<>();
-
-		return new ModelAndView(model, "home/schedule.hbs");
-	}
-
-	public static ModelAndView showWelcome(Request req, Response res) throws Exception {
-		// Just a hash to pass data from the servlet to the page
-		HashMap<String, Object> model = new HashMap<>();
-
-		return new ModelAndView(model, "home/welcome.hbs");
-	}
-
-	public static ModelAndView showRules(Request req, Response res) throws Exception {
-		// Just a hash to pass data from the servlet to the page
-		HashMap<String, Object> model = new HashMap<>();
-
-		return new ModelAndView(model, "home/rules.hbs");
-	}
-
-	public static ModelAndView showAboutMe(Request req, Response res) throws Exception {
-		// Just a hash to pass data from the servlet to the page
-		HashMap<String, Object> model = new HashMap<>();
-
-		return new ModelAndView(model, "home/aboutme.hbs");
-	}
-
-	public static ModelAndView showOther(Request req, Response res) throws Exception {
-		// Just a hash to pass data from the servlet to the page
-		HashMap<String, Object> model = new HashMap<>();
-
-		return new ModelAndView(model, "home/other.hbs");
-	}
-
-	public static ModelAndView showData(Request req, Response res) throws Exception {
-		// Just a hash to pass data from the servlet to the page
-		HashMap<String, Object> model = new HashMap<>();
-
-		return new ModelAndView(model, "home/data.hbs");
-	}
-
-	public static ModelAndView showBehavior(Request req, Response res) throws Exception {
-		// Just a hash to pass data from the servlet to the page
-		HashMap<String, Object> model = new HashMap<>();
-
-		return new ModelAndView(model, "home/behavior.hbs");
-	}
-
-	public static ModelAndView showOurClassroom(Request req, Response res) throws Exception {
-		// Just a hash to pass data from the servlet to the page
-		HashMap<String, Object> model = new HashMap<>();
-
-		return new ModelAndView(model, "home/ourclassroom.hbs");
 	}
 
 }
